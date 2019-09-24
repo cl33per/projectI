e068fb6d33d9718abd4eb0d6cbaab80a286baf6a
karithomas <kari.k.thomas8@gmail.com>
2019-09-24 19:56:55 -0400

    added calendar API

---
+++ (public)/assets/js/calendar.js
@@ -0,0 +1,8 @@
+$(function() {
+  $('#calendar').fullCalendar({
+    googleCalendarApiKey: '<AIzaSyABIl9Bw1hKOBTLMyT2LBvjW210yoEYCXs>',
+    events: {
+      googleCalendarId: 'corporateexecutive0@group.calendar.google.com'
+    }
+  });
+});
