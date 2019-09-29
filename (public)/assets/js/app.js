17dfe9e1cf5f69506eeb2420ac2982c2fbc1c201
cl33per <leeper08@gmail.com>
2019-09-28 21:32:30 -0400

    fixed style for calendar.

--- (public)/assets/js/app.js
+++ (public)/assets/js/app.js
@@ -3,36 +3,38 @@ $(document).ready(function () {
   $("#arrival").hide();
   $("#departures").hide();
   $("#flightPlanner").hide();
+  $("#calendar").hide();
 });
 
-$("#homeBtn").on("click", function () {
-
-});
-
-//$("#calendarBtn").on("click", function() {
-// window.location.href = "../js/calendar.js";
-//console.log ('i am a computer')
+// $("#homeBtn").on("click", function () {
 // });
+
 
 $("#flightBtnA").on("click", function () {
   $("#flightPlanner").show();
   $("#arrivals").show();
   $("#departures").hide();
   $("#chart").hide();
+  $('#calendar').hide();
 });
 
 $("#flightBtnB").on("click", function () {
   $("#flightPlanner").show();
   $("#departures").show();
+   $('#calendar').hdie();
   $("#arrivals").hide();
   $("#chart").hide();
 });
 
+$("#calendarBtn").on("click", function () {
+  $('#calendar').show();
+  $("#flightPlanner").hide();
+  $("#arrivals").hide();
+  $("#departures").hide();
+  $("#chart").hide();
 
+});
 
-//$("#Customers").on("click", function(){
-//console.log('hi');
-//});
 
 $("#reportsBtn").on("click", function () {
   $("#chart").show();
