5a1ad968c5b7588e266a10047c3d3621c5a2027d
cl33per <leeper08@gmail.com>
2019-09-29 17:30:14 -0400

    the final countdown

--- (public)/assets/js/app.js
+++ (public)/assets/js/app.js
@@ -1,45 +1,44 @@
 $(document).ready(function () {
   $('#chart').hide();
-  $("#arrival").hide();
-  $("#departures").hide();
-  $("#flightPlanner").hide();
-  $("#calendar").hide();
-});
-
-// $("#homeBtn").on("click", function () {
-// });
-
-
-$("#flightBtnA").on("click", function () {
-  $("#flightPlanner").show();
-  $("#arrivals").show();
-  $("#departures").hide();
-  $("#chart").hide();
+  $('#arrival').hide();
+  $('#departures').hide();
+  $('#flightPlanner').hide();
   $('#calendar').hide();
 });
 
-$("#flightBtnB").on("click", function () {
-  $("#flightPlanner").show();
-  $("#departures").show();
-   $('#calendar').hdie();
-  $("#arrivals").hide();
-  $("#chart").hide();
+// $('#homeBtn').on('click', function () {
+// });
+
+
+$('#flightBtnA').on('click', function () {
+  $('#flightPlanner').show();
+  $('#arrivals').show();
+  $('#departures').hide();
+  $('#chart').hide();
+  $('#calendar').hide();
 });
 
-$("#calendarBtn").on("click", function () {
+$('#flightBtnB').on('click', function () {
+  $('#flightPlanner').show();
+  $('#departures').show();
+  $('#calendar').hide();
+  $('#arrivals').hide();
+  $('#chart').hide();
+});
+
+$('#calendarBtn').on('click', function () {
   $('#calendar').show();
-  $("#flightPlanner").hide();
-  $("#arrivals").hide();
-  $("#departures").hide();
-  $("#chart").hide();
+  $('#flightPlanner').hide();
+  $('#arrivals').hide();
+  $('#departures').hide();
+  $('#chart').hide();
 
 });
 
-
-$("#reportsBtn").on("click", function () {
-  $("#chart").show();
-  $("#flightPlanner").hide();
-  $("#departures").hide();
-  $("#arrivals").hide();
+$('#reportsBtn').on('click', function () {
+  $('#chart').show();
+  $('#flightPlanner').hide();
+  $('#departures').hide();
+  $('#arrivals').hide();
   $('#calendar').hide();
 });
